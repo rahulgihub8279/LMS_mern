72f252e7bef409d139f52a080dfd341cc70fa150
rahul0008 <149682682+rahulgihub8279@users.noreply.github.com>
2026-07-22 21:36:31 +0530

    Update cookie settings for secure authentication

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -29,8 +29,8 @@ export const signup = async (req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(201).json(user);
@@ -53,8 +53,8 @@ export const login = async (req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.status(201).json({ user });
